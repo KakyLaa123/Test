5307cca1c8e518ef18f921c522f3ab3c9d2da196
Alexandr Kuchmin <kakylaa123@gmail.com>
2021-12-07 15:18:34 +0200

    fixed first commit

--- firstTest.js
+++ firstTest.js
@@ -1,50 +1,27 @@
 class AClass {
-    constructor(n){
-        let Numbers = [];
 
-        function fill(min, max){
-            for(let i = 0; i < n; i++){
-                min = Math.ceil(min);
-                max = Math.ceil(max);
-                let numbersInArray = Math.random() * (max - min) + min;
-                Numbers.push(numbersInArray);
-            }
+    constructor(){
+        if (this.constructor == AClass){
+            throw new Error('Abstract class can not be instantiated');
         }
-
-        let NumbersFactorial = [];
-
-        function factorial(){
-            for(let i = 0; i < n; i++){
-                let numbersInArrayFactorial = (Numbers[i] - 1) * Numbers[i];
-                NumbersFactorial.push(numbersInArrayFactorial);
-            }
-        }
-
-        class Class1 {
-            constructor (){
-                function sort (){
-                    for (let i = 0; i < n - 1; i++){
-                        let maxValue = Numbers[i];
-                            for (let j = i + 1; j < n; j++) {
-                            if (Numbers[j] > maxValue) {
-                                let minValueObj = Numbers[j];
-                                let swapObj = Numbers[i];
-                                Numbers[i] = minValueObj;
-                                Numbers[j] = swapObj;
-                            }
-                        }
-                    }
-                    return Numbers, NumbersFactorial;
-                }
-            }
-        }
-        
-        // class Class2 {
-        //     constructor (){
-        //         function sort (){
-                    
-        //         }
-        //     }
-        // }
     }
+
+    
+
+    fill(max = 10000, min = 0){
+        let Numbers = [];
+        let n = 10;
+        for(let i = 0; i < n; i++){
+            let numbersInArray = Math.round(Math.random() * (max - min) + min);
+            Numbers.push(numbersInArray);
+        }
+        console.log(Numbers);
+    }
+
 }
+
+class Class1 extends AClass {
+
+}
+
+new Class1().fill();
