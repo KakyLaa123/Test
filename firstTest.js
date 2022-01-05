82be082a26c17ef85bbbc21d746faee3c149109e
Alexandr Kuchmin <kakylaa123@gmail.com>
2022-01-05 08:13:58 +0200

    fix

--- firstTest.js
+++ firstTest.js
@@ -1,81 +1,66 @@
-let Numbers;
-
-class AClass {
-    constructor(n){
+class AClass{
+    constructor(Numbers){
         if (this.constructor == AClass){
             throw new Error('Abstract class can not be instantiated');
         }        
-        this.n = n;
-        return n;
+        this.Numbers = Numbers;
+        this.Numbers = new Array(10);
     }
 
-    fill(max = 15, min = 1){
-        Numbers = [];
-        for(let i = 0; i < this.n; i++){
-            let numbersInArray = Math.round(Math.random() * (max - min) + min);
-            Numbers.push(numbersInArray);
+    fill(){
+        for(let i = 0; i < 10; i++){
+            let num = Math.floor(Math.random() * 10);
+            this.Numbers[i] = num;
         }
-        console.log(Numbers);
-        return Numbers;
+        console.log(this.Numbers);
+        return this.Numbers;
     }
 
     factorial(){
-        let NumbersFactorial = [];
-        for(let i = 0; i < Numbers.length; i++){
-            let numbersInFactorial = Numbers[i];
-            for(let j = Numbers[i] - 1; j > 0; j--){
-                numbersInFactorial = numbersInFactorial * j;
-            }
-            NumbersFactorial.push(numbersInFactorial);
-        }
-        console.log(NumbersFactorial);
-        return NumbersFactorial;
-    }
-
-    sort() {
-        throw new Error("Method 'sort()' must be implemented.");
-    }
-}
-
-class Class1 extends AClass {
-    sort(){
-        for(let i = 0; i < Numbers.length - 1; i++){
-            for(let j = 0; j < Numbers.length - 1; j++){
-                if(Numbers[j + 1] < Numbers[j]){
-                    let t = Numbers[j + 1];
-                    Numbers[j + 1] = Numbers[j];
-                    Numbers[j] = t;
+        let factorialArr = new Array(10);
+        for(let i = 0; i < this.Numbers.length; i++){
+            if(this.Numbers[i] == 0 || this.Numbers[i] == 1){
+                factorialArr[i] = 1;
+            }else{
+                let numbersInFactorial = this.Numbers[i];
+                for(let j = this.Numbers[i] - 1; j > 0; j--){
+                    numbersInFactorial = numbersInFactorial * j;
                 }
+                factorialArr[i] = numbersInFactorial;
             }
         }
-        console.log(Numbers);
-        return Numbers;
+        console.log(factorialArr);
+        return factorialArr;
     }
 }
 
-class Class2 extends AClass {
+class Class1 extends AClass{
+    constructor(){
+        super();
+    }
+
     sort(){
-        for(let abc = 0; abc < 2; abc++){
-            for (let i = 0; i < Numbers.length - 1; i++){
-                let maxValue = Numbers[i];
-                    for (let j = i + 1; j < Numbers.length; j++) {
-                    if (Numbers[j] < maxValue) {
-                        let minValue = Numbers[j];
-                        let swap = Numbers[i];
-                        Numbers[i] = minValue;
-                        Numbers[j] = swap;
-                    }
-                }
-            }
-            console.log(Numbers);
-            return Numbers;
-        }
+        this.Numbers.sort((a, b) => a - b);
+        console.log(this.Numbers);
+        return this.Numbers;
     }
 }
 
-new Class1(5).fill();
-new Class1().sort();
-new Class1().factorial();
-new Class2(5).fill();
-new Class2().sort();
-new Class2().factorial();
+class Class2 extends AClass{
+    constructor(){
+        super();
+    }
+
+    sort(){
+        this.Numbers.sort((a, b) => b - a);
+        console.log(this.Numbers);
+        return this.Numbers;
+    }
+}
+
+
+
+const a = new Class2();
+a.fill();
+a.sort();
+a.factorial();
